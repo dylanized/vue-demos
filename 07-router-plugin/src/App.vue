b2eb11001e078d51644948bd460148792fd0d84f
Dylan Hassinger <d@dylanized.com>
2018-05-03 12:55:10 -0500

    added router-plugin demo

---
+++ 07-router-plugin/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id='app'>
+    <ul>
+      <li><router-link to='/'>Home</router-link></li>
+      <li><router-link to='/about'>About</router-link></li>
+      <li><router-link to='/foo'>Foo</router-link></li>
+    </ul>
+    <router-view class='view'></router-view>
+  </div>
+</template>
+
+<script>
+// export component
+export default {
+  name: 'App',
+};
+</script>
---
+++ 07-router-plugin/src/main.js
@@ -0,0 +1,37 @@
+/* eslint-disable no-new */
+
+// load deps
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+// disable dev msg
+Vue.config.productionTip = false;
+
+// load router middleware
+Vue.use(VueRouter);
+
+// define pages
+const Home = { template: '<p>Home page</p>' };
+const About = { template: '<p>About page</p>' };
+const NotFound = { template: '<p>Page not found</p>' };
+
+// instantiate the router and define routes
+const router = new VueRouter({
+  mode: 'history',
+  base: __dirname,
+  routes: [
+    { path: '/', component: Home },
+    { path: '/about', component: About },
+    { path: '*', component: NotFound },
+  ],
+});
+
+// load App component
+import App from './App';
+
+// launch Vue
+new Vue({
+  router,
+  components: { App },
+  template: '<App />',
+}).$mount('main');
