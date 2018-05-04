9314d020106e9a0bb2d8779b7471235fd9142d09
Dylan Hassinger <d@dylanized.com>
2018-05-04 02:12:14 -0500

    added css bundling demo

---
+++ 09-css-bundling/src/components/TodoList.vue
@@ -0,0 +1,25 @@
+<template>
+  <section>
+    <h3>Here is a nested component</h3>
+    <ol>
+      <todo-item
+        v-for='todo in todos'
+        :todo='todo'
+        :key='todo.id'
+      ></todo-item>
+    </ol>
+  </section>
+</template>
+
+<script>
+// import sub components
+import TodoItem from './TodoItem';
+// export TodoList
+export default {
+  name: 'TodoList',
+  components: {
+    TodoItem,
+  },
+  props: ['todos'],
+};
+</script>
