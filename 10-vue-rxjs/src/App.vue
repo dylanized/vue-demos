c851ef118a1405bcde21107058cad22f99182321
Dylan Hassinger <d@dylanized.com>
2018-05-16 15:33:28 -0500

    added rxjs demo

---
+++ 10-vue-rxjs/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id='app'>
+    <h1>Count: {{ count }}</h1>
+  </div>
+</template>
+
+<script>
+// Rx stuff
+import { Observable } from 'rxjs/Observable';
+import interval from 'rxjs/add/observable/interval';
+import filter from 'rxjs/add/operator/filter';
+
+// export App with components
+export default {
+  name: 'App',
+  // define constructor hook
+  created () {
+    // instantiate counter stream
+    const counter$ = Observable.interval(1000);
+    // filter counter for even numbers, then send it to count prop
+    counter$.filter(
+      (value) => value % 2 == 0
+    )
+    .subscribe(
+      (value) => this.count = value
+    )
+  },
+  data() {
+    return {
+      count: 0,
+    };
+  },
+};
+</script>
