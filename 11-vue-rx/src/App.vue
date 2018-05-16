720b69816dcb25fd6d66ea186e74834fb09d1344
Dylan Hassinger <d@dylanized.com>
2018-05-16 15:37:17 -0500

    fixing demo

--- 11-vue-rx/src/App.vue
+++ 11-vue-rx/src/App.vue
@@ -1,11 +1,10 @@
 <template>
   <div id='app'>
-    <h1>Count: {{ count }}</h1>
+    <h1>Count: {{ count$ }}</h1>
   </div>
 </template>
 
 <script>
-// Rx stuff
 import { Observable } from 'rxjs/Observable';
 import interval from 'rxjs/add/observable/interval';
 import filter from 'rxjs/add/operator/filter';
@@ -13,22 +12,10 @@ import filter from 'rxjs/add/operator/filter';
 // export App with components
 export default {
   name: 'App',
-  // define constructor hook
-  created () {
-    // instantiate counter stream
-    const counter$ = Observable.interval(1000);
-    // filter counter for even numbers, then send it to count prop
-    counter$.filter(
-      (value) => value % 2 == 0
-    )
-    .subscribe(
-      (value) => this.count = value
-    )
-  },
-  data() {
+  subscriptions () {
     return {
-      count: 0,
-    };
+      count$: Observable.interval(1000),
+    }
   },
 };
 </script>
--- 11-vue-rx/src/main.js
+++ 11-vue-rx/src/main.js
@@ -2,12 +2,17 @@
 
 // load Vue library
 import Vue from 'vue';
+import VueRx from 'vue-rx';
+import { Observable } from 'rxjs';
 
 // load App component
 import App from './App';
 
 // disable dev msg
 Vue.config.productionTip = false;
+
+// load VueRx
+Vue.use(VueRx, { Observable });
 
 // launch App
 new Vue({
